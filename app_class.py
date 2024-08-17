ae3ea0ea36e416d5b083c6ec81fab01bd0f709a6
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2024-08-18 00:46:20 +0900

    [edit] 클래스 수정
    Session에서 값을 받아오도록 class를 수정

--- app_class.py
+++ app_class.py
@@ -11,27 +11,54 @@ class Session():
         self.result = [int(SessionDB.result1), int(SessionDB.result2), int(SessionDB.result3), int(SessionDB.result4)]
         self.result_text = SessionDB.result_desc
 
+    def update(self, SessionDB):
+        self.token = SessionDB.token
+        self.problem_number = int(SessionDB.problem_num)
+        self.life = int(SessionDB.life)
+        self.description = SessionDB.description
+        self.option = [SessionDB.option1, SessionDB.option2, SessionDB.option3, SessionDB.option4]
+        self.result = [int(SessionDB.result1), int(SessionDB.result2), int(SessionDB.result3), int(SessionDB.result4)]
+        self.result_text = SessionDB.result_desc
+    
+    def to_session_db(self):
+        return SessionDB(
+            token=self.token,
+            problem_num=self.problem_number,
+            life=self.life,
+            description=self.description,
+            option1=self.option[0],
+            option2=self.option[1],
+            option3=self.option[2],
+            option4=self.option[3],
+            result1=self.result[0],
+            result2=self.result[1],
+            result3=self.result[2],
+            result4=self.result[3],
+            result_desc=self.result_text
+        )
+    
+
 
 class Problem():
-    def __init__(self, Session):
-        self.problem_number = 0
-        self.life = 0
+    def __init__(self, Session, select_number):
+        self.problem_number = Session.problem_number
+        self.life = Session.life
         self.image_url = ""
-        self.description = ""
-        self.option = []
+        self.description = Session.description
+        self.option = Session.option
         self.token = Session.token
+        self.select_number = select_number
 
 class Check():
     def __init__(self, Session):
-        self.problem_number = 0
-        self.life = 0
+        self.problem_number = Session.problem_number
+        self.life = Session.life
         self.result_all = False
-        self.result_text = ""
-        self.option = []
+        self.result_text = Session.result_text
+        self.option = Session.option
         self.token = Session.token
-        # Generate attributes from result_1 to result_4
-        for i in range(1, 5):
-            setattr(self, f"result_{i}", 0)
+        self.option = Session.option
+        self.result = Session.result
 
 class Start():
     def __init__(self, Session):
@@ -40,4 +67,4 @@ class Start():
 class Gameover():
     def __init__(self, Session):
         self.token = Session.token
-        self.result_text = ""
+        self.result_text = Session.result_text
