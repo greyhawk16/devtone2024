56ec1bc3b838848cd89cd62323da793fa07d3265
Jeongsik Park <ori@khu.ac.kr>
2024-08-18 08:28:18 +0900

    [edit] paths

--- webpage/gpt.py
+++ webpage/gpt.py
@@ -10,14 +10,14 @@ dotenv.load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 situations_list = []
-with open('D:\\BOB\\TrackStudy\\개발톤\\devtone2024\\webpage\\situation_data.csv', 'r', encoding='utf-8') as file:
+with open('situation_data.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
     for row in reader:
         situations_list.append(row[0])
     file.close()
 
 items_list = []
-with open('D:\\BOB\\TrackStudy\\개발톤\\devtone2024\\webpage\\items_data.csv', 'r', encoding='utf-8') as file:
+with open('items_data.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
     for row in reader:
         items_list.append(row[0])
