1e7a7bf8af0926d1f8bf7e18270f8084b43db599
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2024-08-18 01:27:39 +0900

    [edit] problem_num 명칭 통일
    기존의 `problem_number` 속성 명칭을 `problem_num`으로 통일

--- app_class.py
+++ app_class.py
@@ -4,7 +4,7 @@ from models import SessionDB
 class Session():
     def __init__(self, SessionDB):
         self.token = SessionDB.token
-        self.problem_number = int(SessionDB.problem_num)
+        self.problem_num = int(SessionDB.problem_num)
         self.life = int(SessionDB.life)
         self.description = SessionDB.description
         self.option = [SessionDB.option1, SessionDB.option2, SessionDB.option3, SessionDB.option4]
@@ -13,21 +13,21 @@ class Session():
         self.conv_archive = SessionDB.conv_archive
 
     @staticmethod
-    def create(token, problem_number, life, description, option, result, result_text, conv_archive):
+    def create(token, problem_num, life, description, option, result, result_text, conv_archive):
         return Session(
-            token=token,
-            problem_num=problem_number,
-            life=life,
-            description=description,
-            option=[option[0], option[1], option[2], option[3]],
-            result=[result[0], result[1], result[2], result[3]],
-            result_text=result_text,
+            token = token,
+            problem_num = problem_num,
+            life = life,
+            description = description,
+            option = [option[0], option[1], option[2], option[3]],
+            result = [result[0], result[1], result[2], result[3]],
+            result_text = result_text,
             conv_archive = conv_archive
         )
 
     def update(self, SessionDB):
         self.token = SessionDB.token
-        self.problem_number = int(SessionDB.problem_num)
+        self.problem_num = int(SessionDB.problem_num)
         self.life = int(SessionDB.life)
         self.description = SessionDB.description
         self.option = [SessionDB.option1, SessionDB.option2, SessionDB.option3, SessionDB.option4]
@@ -38,7 +38,7 @@ class Session():
     def to_session_db(self):
         return SessionDB(
             token=self.token,
-            problem_num=self.problem_number,
+            problem_num=self.problem_num,
             life=self.life,
             description=self.description,
             option1=self.option[0],
@@ -56,7 +56,7 @@ class Session():
 
 class Problem():
     def __init__(self, Session, select_number):
-        self.problem_number = Session.problem_number
+        self.problem_num = Session.problem_num
         self.life = Session.life
         self.image_url = ""
         self.description = Session.description
@@ -67,7 +67,7 @@ class Problem():
 
 class Check():
     def __init__(self, Session):
-        self.problem_number = Session.problem_number
+        self.problem_num = Session.problem_num
         self.life = Session.life
         self.result_all = False
         self.result_text = Session.result_text
