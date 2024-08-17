eb883405c1c670393d2a81ff9c9496d4a6cb8657
Jeongsik Park <ori@khu.ac.kr>
2024-08-18 00:22:01 +0900

    [make] model file

---
+++ models.py
@@ -0,0 +1,16 @@
+from . import db
+
+class SessionDB(db.Model):
+    token       = db.Column(db.String(64), primary_key=True)
+    problem_num = db.Column(db.Integer)
+    life        = db.Column(db.Integer)
+    description = db.Column(db.Text)
+    option1     = db.Column(db.Text)
+    option2     = db.Column(db.Text)
+    option3     = db.Column(db.Text)
+    option4     = db.Column(db.Text)
+    result1     = db.Column(db.Integer)
+    result2     = db.Column(db.Integer)
+    result3     = db.Column(db.Integer)
+    result4     = db.Column(db.Integer)
+    result_desc = db.Column(db.Text)
